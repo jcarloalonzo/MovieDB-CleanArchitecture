1b0b66bf50181b24d035f5de20148036b9aed674
Juan Portocarrero <jcarloalonzo@gmail.com>
2023-02-28 10:59:21 -0500

    301-trending-handle-httpfailure

--- lib/app/data/services/remote/trending_api.dart
+++ lib/app/data/services/remote/trending_api.dart
@@ -1,13 +1,17 @@
+import '../../../domain/either/either.dart';
 import '../../../domain/enums.dart';
+import '../../../domain/failures/http_request/http_request_failure.dart';
 import '../../../domain/models/media/media.dart';
 import '../../http/http.dart';
+import '../../utils/handle_failure.dart';
 
 class TrendingAPI {
   TrendingAPI(this._http);
 
   final Http _http;
 
-  getMoviesAndSeries(TimeWindow timeWindow) async {
+  Future<Either<HttpRequestFailure, List<Media>>> getMoviesAndSeries(
+      TimeWindow timeWindow) async {
     final response = await _http.request(
       '/trending/all/${timeWindow.name}',
       onSuccess: (json) {
@@ -20,5 +24,11 @@ class TrendingAPI {
             .toList();
       },
     );
+
+    return response.when(
+        left: (httpFailure) => handleHttpFailure(httpFailure),
+        right: (list) {
+          return Either.right(list);
+        });
   }
 }
---
+++ lib/app/data/utils/handle_failure.dart
@@ -0,0 +1,23 @@
+import '../../domain/either/either.dart';
+import '../../domain/failures/http_request/http_request_failure.dart';
+import '../http/failure.dart';
+
+Either<HttpRequestFailure, R> handleHttpFailure<R>(HttpFailure httpFailure) {
+  final failure = () {
+    final statusCode = httpFailure.statusCode;
+
+    switch (statusCode) {
+      case 404:
+        return HttpRequestFailure.notFound();
+      case 401:
+        return HttpRequestFailure.unauthorized();
+    }
+
+    if (httpFailure.exception is NetworkException) {
+      return HttpRequestFailure.network();
+    }
+    return HttpRequestFailure.unknow();
+  }();
+
+  return Either.left(failure);
+}
