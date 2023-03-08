c58027d6e09111b0a84213ad0d57b509562b2d45
Juan Portocarrero <jcarloalonzo@gmail.com>
2023-03-08 01:26:00 -0500

    336 moview view part 5

--- lib/app/presentation/pages/movie/components/movie_cast.dart
+++ lib/app/presentation/pages/movie/components/movie_cast.dart
@@ -1,3 +1,4 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -5,6 +6,8 @@ import '../../../../domain/either/either.dart';
 import '../../../../domain/failures/http_request/http_request_failure.dart';
 import '../../../../domain/models/actors/actors.dart';
 import '../../../../domain/repositories/movies_repository.dart';
+import '../../../global/widgets/request_failed.dart';
+import '../../../utils/get_image_url.dart';
 
 class MovieCast extends StatefulWidget {
   const MovieCast({super.key, required this.movieID});
@@ -42,7 +45,71 @@ class _MovieCastState extends State<MovieCast> {
             child: CircularProgressIndicator(),
           );
         }
-        return const Text('00');
+        return snapshot.data!.when(
+          left: (_) {
+            return RequestFailed(
+              onRetry: () {
+                setState(() {
+                  _initFuture();
+                });
+              },
+            );
+          },
+          right: (list) {
+            return Column(
+              children: [
+                const Text(
+                  'Cast',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  child: ListView.separated(
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(width: 10);
+                    },
+                    scrollDirection: Axis.horizontal,
+                    itemCount: list.length,
+                    itemBuilder: (_, index) {
+                      final actor = list[index];
+                      return Column(
+                        children: [
+                          Expanded(
+                            child: LayoutBuilder(
+                              builder: (_, contraints) {
+                                final size = contraints.maxHeight;
+                                return ClipRRect(
+                                  borderRadius: BorderRadius.circular(size / 2),
+                                  child: ExtendedImage.network(
+                                    getImageURL(actor.profilePath!),
+                                    height: size,
+                                    width: size,
+                                    fit: BoxFit.cover,
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            actor.name ?? '',
+                            style: const TextStyle(fontSize: 11),
+                          ),
+                        ],
+                      );
+
+                      //
+                    },
+                  ),
+                )
+              ],
+            );
+            //
+          },
+        );
       },
     );
   }
