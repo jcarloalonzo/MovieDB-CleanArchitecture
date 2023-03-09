424a2683bdb2221860c09867ac881b6b8d034527
Juan Portocarrero <jcarloalonzo@gmail.com>
2023-03-09 00:48:53 -0500

    344 favoritos part5

---
+++ lib/app/presentation/pages/favorites/components/favorites_appbar.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class FavoritesAppBar extends StatelessWidget with PreferredSizeWidget {
+  const FavoritesAppBar({super.key, required this.tabController});
+  final TabController tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      titleTextStyle: const TextStyle(color: Colors.black),
+      title: const Text('Favorites'),
+      centerTitle: true,
+      iconTheme: const IconThemeData(
+        color: Colors.black,
+      ),
+      elevation: 0,
+      backgroundColor: Colors.white,
+      bottom: TabBar(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        indicatorSize: TabBarIndicatorSize.label,
+        // indicator: BoxDecoration(
+        //   color: Colors.black12,
+        //   borderRadius: BorderRadius.circular(30),
+        // ),
+        indicator: const _Decoration(color: Colors.blue, width: 20),
+        controller: tabController,
+        labelColor: Colors.black,
+        tabs: const [
+          SizedBox(
+            height: 30,
+            child: Tab(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                child: Text('Movies'),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+            child: Tab(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                child: Text('Series'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight * 2);
+}
+
+class _Decoration extends Decoration {
+  const _Decoration({required this.color, required this.width});
+
+  final Color color;
+  final double width;
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) =>
+      _Painter(color, width);
+}
+
+class _Painter extends BoxPainter {
+  _Painter(this.color, this.width);
+
+  final Color color;
+  final double width;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    //
+    final paint = Paint()..color = color;
+    final size = configuration.size ?? Size.zero;
+
+    canvas.drawRRect(
+        RRect.fromRectAndRadius(
+            Rect.fromLTWH(size.width * 0.5 + offset.dx - width * 0.5,
+                size.height * 0.9, width, width * 0.3),
+            const Radius.circular(4)),
+        paint);
+
+    // canvas.drawCircle(offset, 4, paint);
+    // canvas.drawCircle(
+    //     Offset(size.width * 0.5 + offset.dx, size.height), 4, paint);
+  }
+}
--- lib/app/presentation/pages/favorites/favorites_page.dart
+++ lib/app/presentation/pages/favorites/favorites_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../../global/controller/favorites/favorites_controller.dart';
 import '../../global/widgets/request_failed.dart';
+import 'components/favorites_appbar.dart';
 import 'components/favorites_content.dart';
 
 class FavoritesPage extends StatefulWidget {
@@ -32,46 +33,7 @@ class _FavoritesPageState extends State<FavoritesPage>
     final bloc = context.watch<FavoritesController>();
 
     return Scaffold(
-      appBar: AppBar(
-        titleTextStyle: const TextStyle(color: Colors.black),
-        title: const Text('Favorites'),
-        centerTitle: true,
-        iconTheme: const IconThemeData(
-          color: Colors.black,
-        ),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        bottom: TabBar(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          indicatorSize: TabBarIndicatorSize.label,
-          indicator: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          controller: _tabController,
-          labelColor: Colors.black,
-          tabs: const [
-            SizedBox(
-              height: 30,
-              child: Tab(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Text('Movies'),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30,
-              child: Tab(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Text('Series'),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      appBar: FavoritesAppBar(tabController: _tabController),
       body: bloc.state.map(
         loading: (_) {
           return const CircularProgressIndicator();
